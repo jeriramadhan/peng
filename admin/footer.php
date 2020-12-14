a15f59f1131acdf9fa2b94b30aa8f7c0878153c0
Jeri Ramadhan <jeriramadhan25@gmail.com>
2020-12-14 22:01:06 +0700

    push awal

---
+++ admin/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+	<div class="pull-right hidden-xs">
+		<b> Aplikasi e-Kurir </b> 
+	</div>
+	<strong>Copyright &copy; 2020 <a href="">Dani</a></strong>  | 
+</footer>
---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="row">
+		<footer>
+			<div class="pull-right hidden-xs">
+				<b> Aplikasi e-Kurir </b> 
+			</div>
+			<strong>Copyright &copy; 2020 <a href="">Dani</a></strong> 
+		</footer>
+	</div>
+</div>
